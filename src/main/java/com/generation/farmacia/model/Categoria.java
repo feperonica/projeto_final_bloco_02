bfba309508d5e0448b0b9da8d7df16151b8f3bd6
Felipe Peronica <felipeperonica@gmail.com>
2025-05-28 12:03:59 -0300

    :sparkles: CRUD Categoria implementado

---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,64 @@
+package com.generation.farmacia.model;
+
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O atributo nome é obrigatório!")
+    @Size(min = 3, max = 100, message = "O nome deve ter entre 3 e 100 caracteres")
+    private String nome;
+
+    @NotBlank(message = "O atributo descrição é obrigatório!")
+    @Size(min = 5, max = 500, message = "A descrição deve ter entre 5 e 500 caracteres")
+    private String descricao;
+
+    /* @OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+    @JsonIgnoreProperties("categoria")
+    private List<Produto> produtos; */
+    
+    // Getters e Setters
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	/* public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}*/
+    
+	
+}
